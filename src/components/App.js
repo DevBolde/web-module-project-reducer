3243256670819d75e3026686cabacd27e18997ba
Devin Bolden <devinbolden6@gmail.com>
2023-04-27 13:56:24 -0500

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,13 @@ import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
 import reducer, {initialState} from '../reducers/index'
-import { ADD_ONE } from '../actions';
+import { addOne } from '../actions';
 
 function App() {
   const[ state, dispatch ] = useReducer(reducer, initialState)
+const handle1click =() => {
+  dispatch(addOne());
+}
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -33,7 +36,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={handle1click}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
